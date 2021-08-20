505a492b6ce084c12707a8c17d07598d77d97b6b
LP <luizpaulobborges@gmail.com>
2021-08-20 17:34:40 -0300

    feat(pages/posts): consuming prismic api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import logo from '../../../public/images/logo.svg';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image src={logo} alt="ig.news" />
         <nav>
-          <a className={styles.active} href="#">Home</a>
-          <a href="#">Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         
         <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -10,41 +26,45 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>
-              20 de agosto de 2021
-            </time>
-            <strong>
-              Mapas com React usando Leaflet
-            </strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.
-            </p>
-          </a>
-          <a>
-            <time>
-              20 de agosto de 2021
-            </time>
-            <strong>
-              Mapas com React usando Leaflet
-            </strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.
-            </p>
-          </a>
-          <a>
-            <time>
-              20 de agosto de 2021
-            </time>
-            <strong>
-              Mapas com React usando Leaflet
-            </strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.
-            </p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  });
+
+  return {
+    props: {
+      posts,
+    }
+  }
 }
