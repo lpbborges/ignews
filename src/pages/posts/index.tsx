bf8f542077121ed9600ccef6ac0670a23e024e44
LP <luizpaulobborges@gmail.com>
2021-08-20 15:47:40 -0300

    feat(pages/posts): posts pages layout

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>
+              20 de agosto de 2021
+            </time>
+            <strong>
+              Mapas com React usando Leaflet
+            </strong>
+            <p>
+              Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.
+            </p>
+          </a>
+          <a>
+            <time>
+              20 de agosto de 2021
+            </time>
+            <strong>
+              Mapas com React usando Leaflet
+            </strong>
+            <p>
+              Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.
+            </p>
+          </a>
+          <a>
+            <time>
+              20 de agosto de 2021
+            </time>
+            <strong>
+              Mapas com React usando Leaflet
+            </strong>
+            <p>
+              Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
